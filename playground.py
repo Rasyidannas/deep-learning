3b55e701dc06736d99cdb4aa95c1deef49a25eb8
rasyidannas <rasshit.dsgn@gmail.com>
2026-04-20 04:28:21 +0000

    prediction with multiple outputs

--- playground.py
+++ playground.py
@@ -58,7 +58,28 @@ nfans = np.array([1.2, 1.3, 0.5, 1.0])
 input = np.array([toes[0], wrlec[0], nfans[0]])
 pred = neural_network(input, weights)
 print(pred)
-# -
+# +
+# Using one input for multiple outputs
+weights = [0.3, 0.2, 0.9]
 
+def ele_mul(number, vector):
+    output = [0, 0, 0]
+    assert(len(output) == len(vector))
+    
+    for i in range(len(vector)):
+        output[i] = number * vector[i]
+        
+    return output
+
+def neural_network(input, weights):
+    pred = ele_mul(input, weights)
+    return pred
+
+wlrec = [0.65, 0.8, 0.8, 0.9]
+input = wlrec[0]
+    
+pred = neural_network(input, weights)
+print(pred)
+# -
 
 
